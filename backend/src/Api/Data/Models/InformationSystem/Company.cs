2eda26cfc6f86a5e98d8d90cf0ee695dd316bd27
Filip Hlavačka <piko123piko@gmail.com>
2024-10-24 21:28:32 +0200

    InformationSystem models

--- backend/src/Api/Data/Models/InformationSystem/Company.cs
+++ backend/src/Api/Data/Models/InformationSystem/Company.cs
@@ -5,14 +5,12 @@ public class Company
     public int Id { get; set; }
     public string Title { get; set; }
     public string Title2 { get; set; }
-    public string IC { get; set; }
-    public string DIC { get; set; }
+    public string Ic { get; set; }
+    public string Dic { get; set; }
     public string Ulice { get; set; }
-    public string PSC { get; set; }
+    public string Psc { get; set; }
     public string City { get; set; }
-
-    // mozno + lics orders
+    
     public List<DeliveryNote> DeliveryNotes { get; set; } = new List<DeliveryNote>();
     public List<Invoice> Invoices { get; set; } = new List<Invoice>();
-    public List<WorkReport> WorkReports { get; set; } = new List<WorkReport>();
 }
