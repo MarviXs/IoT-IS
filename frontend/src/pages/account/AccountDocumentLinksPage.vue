eddb706083acceb784faca68e88b54790643f873
Martin Raffáč <mraffac@gmail.com>
2025-03-09 23:33:54 +0100

    pridanie sekcie Documents Links

---
+++ frontend/src/pages/account/AccountDocumentLinksPage.vue
@@ -0,0 +1,48 @@
+<template>
+    <page-layout :breadcrumbs="[{ label: t('account.document_links') }]">
+      <q-card class="shadow q-mb-lg">
+        <q-tabs dense class="text-grey" active-color="primary" indicator-color="primary" align="left">
+          <q-route-tab
+            :to="{ path: '/account' }"
+            style="min-width: 130px"
+            name="account"
+            :icon="mdiAccount"
+            :label="t('account.label')"
+            no-caps
+          />
+          <q-route-tab
+            :to="{ path: '/account/document-links' }"
+            style="min-width: 130px"
+            name="document-links"
+            :icon="mdiFileDocument"
+            :label="t('account.document_links')"
+            no-caps
+          />
+        </q-tabs>
+      </q-card>
+      <document-link-card
+        document-type="Payroll"
+        :loading="loading"
+        @onSubmit="handleDocumentLinkSubmit"
+      />
+    </page-layout>
+  </template>
+  
+  <script setup lang="ts">
+
+  import { useI18n } from 'vue-i18n';
+  import { ref } from 'vue';
+  import { mdiAccount, mdiFileDocument } from '@quasar/extras/mdi-v7';
+  import PageLayout from '@/layouts/PageLayout.vue';
+  import DocumentLinkCard from '@/components/account/DocumentLinkCard.vue';
+  
+  const loading = ref(false);
+  const { t } = useI18n();
+  
+  function handleDocumentLinkSubmit(link: string) {
+    loading.value = true;
+    // Handle the submission logic here
+    console.log('Document Link Submitted:', link);
+    loading.value = false;
+  }
+  </script>
