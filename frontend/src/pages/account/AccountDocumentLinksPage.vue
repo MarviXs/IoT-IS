b4b7dd7a6ae0e772df48af280ea91326a9a66047
Martin Raffáč <mraffac@gmail.com>
2025-03-26 06:24:30 +0100

    Add search and add document button

--- frontend/src/pages/account/AccountDocumentLinksPage.vue
+++ frontend/src/pages/account/AccountDocumentLinksPage.vue
@@ -20,6 +20,21 @@
           />
         </q-tabs>
       </q-card>
+
+      <div class="q-mb-md row items-center q-gutter-sm justify-end">
+        <SearchBar v-model="filter" class="col-grow col-lg-auto" />
+        <q-btn
+          class="shadow col-grow col-lg-auto"
+          color="primary"
+          unelevated
+          no-caps
+          size="15px"
+          :label="t('account.add_document')"
+          :icon="mdiPlus"
+          @click="isCreateDialogOpen = true"
+        />
+      </div>
+
       <document-link-card
         :document-type="t('account.payroll')"
         :loading="loading"
@@ -36,18 +51,27 @@
         @onSubmit="handleDocumentLinkSubmit"
       />
     </page-layout>
+    <CreateDocumentDialog v-model="isCreateDialogOpen"  />
   </template>
   
   <script setup lang="ts">
 
   import { useI18n } from 'vue-i18n';
   import { ref } from 'vue';
+  import { mdiPlus } from '@quasar/extras/mdi-v7';
+  import SearchBar from '@/components/core/SearchBar.vue';
   import { mdiAccount, mdiFileDocument } from '@quasar/extras/mdi-v7';
   import PageLayout from '@/layouts/PageLayout.vue';
   import DocumentLinkCard from '@/components/account/DocumentLinkCard.vue';
+  import CreateDocumentDialog from '@/components/account/CreateDocumentDialog.vue';
+  //import { PaginationClient, PaginationTable } from '@/models/Pagination';
+
   
   const loading = ref(false);
   const { t } = useI18n();
+  const filter = ref('');
+  const isCreateDialogOpen = ref(false);
+
   
   function handleDocumentLinkSubmit(link: string) {
     loading.value = true;
@@ -55,4 +79,7 @@
     console.log('Document Link Submitted:', link);
     loading.value = false;
   }
+
+  
+
   </script>
