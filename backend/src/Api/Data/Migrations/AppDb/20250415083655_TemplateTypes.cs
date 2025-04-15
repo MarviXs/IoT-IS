65ee7221105fc37687859e097dca59791eb10a70
Marek Smogrovic <smogrovic.marek@gmail.com>
2025-04-15 10:37:43 +0200

    Add migrations

---
+++ backend/src/Api/Data/Migrations/AppDb/20250415083655_TemplateTypes.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Fei.Is.Api.Data.Migrations.AppDb
+{
+    /// <inheritdoc />
+    public partial class TemplateTypes : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Group",
+                table: "Sensors",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "Order",
+                table: "Sensors",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "DeviceType",
+                table: "DeviceTemplates",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Group",
+                table: "Sensors");
+
+            migrationBuilder.DropColumn(
+                name: "Order",
+                table: "Sensors");
+
+            migrationBuilder.DropColumn(
+                name: "DeviceType",
+                table: "DeviceTemplates");
+        }
+    }
+}
