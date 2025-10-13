150b50ab3cedf8d600e5455caaa874d1e8b6e37d
Marek Smogrovic <smogrovic.marek@gmail.com>
2025-10-13 20:49:01 +0200

    Job schedule migration

---
+++ backend/src/Api/Data/Migrations/AppDb/20251013184839_JobSchedules.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Fei.Is.Api.Data.Migrations.AppDb
+{
+    /// <inheritdoc />
+    public partial class JobSchedules : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "JobSchedules",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    DeviceId = table.Column<Guid>(type: "uuid", nullable: false),
+                    RecipeId = table.Column<Guid>(type: "uuid", nullable: false),
+                    Type = table.Column<int>(type: "integer", nullable: false),
+                    Interval = table.Column<int>(type: "integer", nullable: true),
+                    IntervalValue = table.Column<int>(type: "integer", nullable: true),
+                    StartTime = table.Column<DateTimeOffset>(type: "timestamp with time zone", nullable: false),
+                    EndTime = table.Column<DateTimeOffset>(type: "timestamp with time zone", nullable: true),
+                    Cycles = table.Column<int>(type: "integer", nullable: false),
+                    IsActive = table.Column<bool>(type: "boolean", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    UpdatedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_JobSchedules", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_JobSchedules_Devices_DeviceId",
+                        column: x => x.DeviceId,
+                        principalTable: "Devices",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_JobSchedules_Recipes_RecipeId",
+                        column: x => x.RecipeId,
+                        principalTable: "Recipes",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "JobScheduleWeekDays",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    JobScheduleId = table.Column<Guid>(type: "uuid", nullable: false),
+                    Day = table.Column<int>(type: "integer", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    UpdatedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_JobScheduleWeekDays", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_JobScheduleWeekDays_JobSchedules_JobScheduleId",
+                        column: x => x.JobScheduleId,
+                        principalTable: "JobSchedules",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_JobScheduleWeekDays_JobScheduleId_Day",
+                table: "JobScheduleWeekDays",
+                columns: new[] { "JobScheduleId", "Day" },
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_JobSchedules_DeviceId",
+                table: "JobSchedules",
+                column: "DeviceId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_JobSchedules_RecipeId",
+                table: "JobSchedules",
+                column: "RecipeId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "JobScheduleWeekDays");
+
+            migrationBuilder.DropTable(
+                name: "JobSchedules");
+        }
+    }
+}
