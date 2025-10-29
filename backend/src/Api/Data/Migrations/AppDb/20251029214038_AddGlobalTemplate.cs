452d90c1c15188e2c55fe0f3d62369003223f64b
Marek Smogrovic <smogrovic.marek@gmail.com>
2025-10-29 22:43:29 +0100

    add missing migration

---
+++ backend/src/Api/Data/Migrations/AppDb/20251029214038_AddGlobalTemplate.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Fei.Is.Api.Data.Migrations.AppDb
+{
+    /// <inheritdoc />
+    public partial class AddGlobalTemplate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsGlobal",
+                table: "DeviceTemplates",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsGlobal",
+                table: "DeviceTemplates");
+        }
+    }
+}
